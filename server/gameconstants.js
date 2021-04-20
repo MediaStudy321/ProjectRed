f1e5da7b4d26a311a6b1f0cc4e1fea97f600cae5
bryantme <78558289+bryantme@users.noreply.github.com>
2021-04-20 15:41:39 -0400

    class combat tutorial

---
+++ server/gameconstants.js
@@ -0,0 +1,26 @@
+const sampleHeroes = {
+    Yourcharactersname: {
+        id: 1,
+        name: 'Cloud',
+        attack: 30,
+        max_hp: 300,
+        max_mp: 200,
+        speed: 1,
+        color: '#FF0000'
+    },
+}
+
+const sampleMonsters = {
+    Monstersname: {
+        id: 1,
+        name: 'Ferocious rabbit',
+        attack: 1,
+        max_hp: 50,
+        max_mp: 0,
+        speed: 18,
+        icon: '&#9752;',
+        color: '#DDFFDD'
+    },
+}
+
+module.exports = {sampleHeroes, sampleMonsters}
