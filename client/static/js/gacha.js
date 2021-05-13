ed642129e79a68e4b5d4fa1016c812a3cd18c60e
ElijahStrauss <emstraus@buffalo.edu>
2021-05-12 21:00:47 -0400

    work on gacha mechanic

--- client/static/js/gacha.js
+++ client/static/js/gacha.js
@@ -4,10 +4,11 @@ $(async ()=>{
     async function load() {
         try {
             await $.ajax({
-                url: 'getcharacters',
+                url: 'getallheroes',
                 type: 'GET',
-                success: (data) => {
-                    characters=data;
+                success: (data) =>{
+                    heroSet=data;
+                    displayControls()
                 }
             });
         }
@@ -16,6 +17,36 @@ $(async ()=>{
             window.alert("I'm sorry, the server is down!");
         }
     }
+
+    function randomHero() {
+		let choice=Math.floor(Math.random()*heroSet.length);
+		return heroSet[choice];
+      }
+
+    function displayControls(){
+        $('#banner').html("Reach into the abyss and claim your champions!");
+        $('#controls').html('');
+
+        //Rolls
+        $('#controls').append("<button id='abyssRoll'>The Abyss</button>");
+        $('#abyssRoll').on('click', ()=>{
+            randomHero(heroSet);
+            $('#banner').html('You pulled'+ hero);
+            $('#controls').append("<button id='confirm'>Confirm</button>");
+                $('#confirm').on('click', ()=>{
+                    console.log(hero)
+                    $.ajax({
+                     type: "POST",
+                     data: hero,
+                     url: "heroPull",
+                     success: (data) => {
+                         alert(hero + 'was added to your hero pool!')
+                        }
+                    });
+                    displayControls(heroSet);
+                });
+            });
+        }
 	//  Party SET-UP
     await load();
 
